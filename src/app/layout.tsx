923aa2727bc7278e70ad60f6a6f2c34d61ea2b01
Peter Phanouvong <peterphanouvong@gmail.com>
2025-07-21 14:36:42 +1000

    new setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
-import "./globals.css";
-import {
-  RegisterLink,
-  LoginLink,
-  LogoutLink,
-} from "@kinde-oss/kinde-auth-nextjs/components";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import Link from "next/link";
 import { AuthProvider } from "./AuthProvider";
-
-export const metadata = {
-  title: "Kinde Auth",
-  description: "Kinde with NextJS App Router",
-};
+import "./globals.css";
 
 export default async function RootLayout({
   children,
@@ -25,34 +14,8 @@ export default async function RootLayout({
       <html lang="en">
         <body>
           <header>
-            <nav className="">
-              <h1 className="">Application name</h1>
-              <div>
-                {!(await isAuthenticated()) ? (
-                  <>
-                    <LoginLink className="">Sign in</LoginLink>
-                    <RegisterLink className="">Sign up</RegisterLink>
-                  </>
-                ) : (
-                  <div className="">
-                    {user?.picture ? (
-                      "hi"
-                    ) : (
-                      <div className="">
-                        {user?.given_name?.[0]}
-                        {user?.family_name?.[0]}
-                      </div>
-                    )}
-                    <div>
-                      <p className="">
-                        {user?.given_name} {user?.family_name}
-                      </p>
-
-                      <LogoutLink className="">Log out</LogoutLink>
-                    </div>
-                  </div>
-                )}
-              </div>
+            <nav>
+              <div></div>
             </nav>
           </header>
           <main>{children}</main>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <div className="bg-red-500"> REPLACE THIS WITH A LANDING PAGE</div>;
+  return <div></div>;
 }
