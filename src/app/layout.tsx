1dc428740897cf3fbe094ece1a074a11a5e7e6fe
Koosha Owji <koosha.owji@gmail.com>
2025-09-16 20:59:05 +1000

    chore: wrap the whole layout with kinde Provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,62 +23,62 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body>
-        <header>
-          <nav className="nav container">
-            <h1 className="text-display-3">KindeAuth</h1>
-            <div>
-              {!(await isAuthenticated()) ? (
-                <>
-                  <LoginLink className="btn btn-ghost sign-in-btn">
-                    Sign in
-                  </LoginLink>
-                  <RegisterLink className="btn btn-dark">Sign up</RegisterLink>
-                </>
-              ) : (
-                <div className="profile-blob">
-                  {user?.picture ? (
-                    <img
-                      className="avatar"
-                      src={user?.picture}
-                      alt="user profile avatar"
-                      referrerPolicy="no-referrer"
-                    />
-                  ) : (
-                    <div className="avatar">
-                      {user?.given_name?.[0]}
-                      {user?.family_name?.[0]}
+        <KindeProvider>
+          <header>
+            <nav className="nav container">
+              <h1 className="text-display-3">KindeAuth</h1>
+              <div>
+                {!(await isAuthenticated()) ? (
+                  <>
+                    <LoginLink className="btn btn-ghost sign-in-btn">
+                      Sign in
+                    </LoginLink>
+                    <RegisterLink className="btn btn-dark">Sign up</RegisterLink>
+                  </>
+                ) : (
+                  <div className="profile-blob">
+                    {user?.picture ? (
+                      <img
+                        className="avatar"
+                        src={user?.picture}
+                        alt="user profile avatar"
+                        referrerPolicy="no-referrer"
+                      />
+                    ) : (
+                      <div className="avatar">
+                        {user?.given_name?.[0]}
+                        {user?.family_name?.[0]}
+                      </div>
+                    )}
+                    <div>
+                      <p className="text-heading-2">
+                        {user?.given_name} {user?.family_name}
+                      </p>
+
+                      <LogoutLink className="text-subtle">Log out</LogoutLink>
                     </div>
-                  )}
-                  <div>
-                    <p className="text-heading-2">
-                      {user?.given_name} {user?.family_name}
-                    </p>
-
-                    <LogoutLink className="text-subtle">Log out</LogoutLink>
                   </div>
-                </div>
-              )}
-            </div>
-          </nav>
-        </header>
-        <main>
-          <KindeProvider>{children}</KindeProvider>
-        </main>
-        <footer className="footer">
-          <div className="container">
-            <strong className="text-heading-2">KindeAuth</strong>
-            <p className="footer-tagline text-body-3">
-              Visit our{" "}
-              <Link className="link" href="https://kinde.com/docs">
-                help center
-              </Link>
-            </p>
+                )}
+              </div>
+            </nav>
+          </header>
+          <main>{children}</main>
+          <footer className="footer">
+            <div className="container">
+              <strong className="text-heading-2">KindeAuth</strong>
+              <p className="footer-tagline text-body-3">
+                Visit our{" "}
+                <Link className="link" href="https://kinde.com/docs">
+                  help center
+                </Link>
+              </p>
 
-            <small className="text-subtle">
-              © 2023 KindeAuth, Inc. All rights reserved
-            </small>
-          </div>
-        </footer>
+              <small className="text-subtle">
+                © 2023 KindeAuth, Inc. All rights reserved
+              </small>
+            </div>
+          </footer>
+        </KindeProvider>
       </body>
     </html>
   );
